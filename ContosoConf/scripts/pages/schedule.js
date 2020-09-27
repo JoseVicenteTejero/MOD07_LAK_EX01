52ea0413d5321e155534fbf4d404eeba5965df46
José Vicente <josevicentetejero@yahoo.es>
2020-09-27 20:44:46 +0200

    A

--- ContosoConf/scripts/pages/schedule.js
+++ ContosoConf/scripts/pages/schedule.js
@@ -3,19 +3,23 @@
 /// <reference path="../datetime.js" />
 
 //TODO: Import objects/functions from the modules/classes.
-import { LocalStarStorage } from "../LocalStarStorage.js";
+/*
+ import { LocalStarStorage } from "../LocalStarStorage.js";
 import { ScheduleItem } from "../ScheduleItem.js";
-
-// TODO: Create a ScheduleList class
+*/
+import { LocalStarStorage } from "../LocalStarStorage.js";
+import { ScheduleList } from "../ScheduleList.js";
 
 // TODO: Refactor these variables into properties of the ScheduleList class.
 //       Assign them in the "initialize" method from arguments
 
-const element, localStarStorage;
+// const element, localStarStorage;
 
 // TODO: Refactor these functions into methods of the ScheduleList class.
 
-async function startDownload() {
+
+
+/* async function startDownload() {
     // await response of fetch call
     let response = await fetch("/schedule/list")
     // transform body to json
@@ -36,7 +40,7 @@ function downloadDone(responseData) {
 function downloadFailed() {
     alert("Could not retrieve schedule data at this time. Please try again later.");
 }
-
+*/ 
 function addAll(itemsArray) {
     itemsArray.forEach(add); // TODO: When refactoring this, add the `this` argument to `forEach`.
 }
@@ -48,9 +52,16 @@ function add(itemData) {
 
 // TODO: Replace the following code by creating a ScheduleList object 
 //       and calling the startDownload method.
-element = document.getElementById("schedule");
+/*element = document.getElementById("schedule");
 localStarStorage = new LocalStarStorage(localStorage);
-startDownload();
+startDownload();*/
+
+const scheduleList = new ScheduleList(
+    document.getElementById("schedule"),
+    new LocalStarStorage(localStorage)
+);
+scheduleList.startDownload();
+
 // SIG // Begin signature block
 // SIG // MIIaVgYJKoZIhvcNAQcCoIIaRzCCGkMCAQExCzAJBgUr
 // SIG // DgMCGgUAMGcGCisGAQQBgjcCAQSgWTBXMDIGCisGAQQB
