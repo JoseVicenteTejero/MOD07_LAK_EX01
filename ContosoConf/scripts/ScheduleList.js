6eeeb1882b45f615abf1dcdef820d1ada333e0ac
José Vicente <josevicentetejero@yahoo.es>
2020-09-28 16:04:49 +0200

    a

--- ContosoConf/scripts/ScheduleList.js
+++ ContosoConf/scripts/ScheduleList.js
@@ -1,4 +1,4 @@
-﻿```javascript
+﻿// javascript
 	import { ScheduleItem } from "./ScheduleItem.js";
 
 	export class ScheduleList {
@@ -7,36 +7,37 @@
 			this.element = element;
 			this.localStarStorage = localStarStorage;
 		}
+		async startDownload() {
+			// await response of fetch call
+			let response = await fetch("/schedule/list")
+			// transform body to json
+			let data = await response.json();
+
+			// checking response is ok
+			if (response.ok) {
+				this.downloadDone(data);
+			} else {
+				this.downloadFailed();
+			}
+		}
+
+		downloadDone(responseData) {
+			this.addAll(responseData.schedule);
+		}
+
+		downloadFailed() {
+			alert("Could not retrieve schedule data at this time. Please try again later.");
+		}
+
+		addAll(itemsArray) {
+			itemsArray.forEach(this.add, this);
+		}
+
+		add(itemData) {
+			const item = new ScheduleItem(itemData, this.localStarStorage);
+			this.element.appendChild(item.element);
+		}
 	}
 	
 	//TODO: Add methods
-	async startDownload() {
-		// await response of fetch call
-		let response = await fetch("/schedule/list")
-		// transform body to json
-		let data = await response.json();
 
-		// checking response is ok
-		if (response.ok) {
-			this.downloadDone(data);
-		} else {
-			this.downloadFailed();
-		}
-	}
-
-	downloadDone(responseData) {
-		this.addAll(responseData.schedule);
-	}
-
-	downloadFailed() {
-		alert("Could not retrieve schedule data at this time. Please try again later.");
-	}
-
-	addAll(itemsArray) {
-		itemsArray.forEach(this.add, this);
-	}
-
-	add(itemData) {
-		const item = new ScheduleItem(itemData, this.localStarStorage);
-		this.element.appendChild(item.element);
-	}
